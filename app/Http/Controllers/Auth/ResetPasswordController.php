8b3ee4cb7fc7e1174afd5769c0dff651ec868347
ElMedusita <alexisgilcabrera@gmail.com>
2022-01-23 12:26:07 +0000

    Restablecer contraseña

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
-use App\Models\User;
 
 class ResetPasswordController extends Controller
 {
@@ -28,13 +27,4 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
-    
-    protected function redirectTo(){
-        if(Auth()->user()->role == 1 ){
-            return route('admin.dashboard');
-        }
-        elseif(Auth()->user()->role == 2){
-            return route('user.dashboard');
-        }
-    }
 }
